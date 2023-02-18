5af33736b16143080c4f4bb6ac55a20cb481db8d
Neruthes <i@neruthes.xyz>
2023-02-18 22:28:07 +0000

    Regular update '2023-02-18T22:28:07+00:00'

--- src/main/201-libcontacts.sh
+++ src/main/201-libcontacts.sh
@@ -7,7 +7,7 @@
 #
 # Contacts are saved in '$CONFDIR/contacts-v1.psv'
 # It is a LF-delimited text file consisting of lines such as:
-#       pkid:age:age1145141919810|John Appleseed
+#       John Appleseed|kpid:age:age1145141919810
 
 
 CONTACTSDBFILE1="$CONFDIR/contacts-v1.psv"
@@ -16,8 +16,8 @@ function contacts_v1_initdb() {
     touch "$CONTACTSDBFILE1"
 }
 function contacts_v1_search() {
-    ( grep -E "|$1$" "$CONTACTSDBFILE1"
-    grep -E "$1|" "$CONTACTSDBFILE1" ) | head -n1
+    ( grep -E "|$1" "$CONTACTSDBFILE1"
+    grep -E "^$1|" "$CONTACTSDBFILE1" ) | head -n1
 }
 function contacts_v1_insert() {
     echo "$1|$2" >> "$CONTACTSDBFILE1"
